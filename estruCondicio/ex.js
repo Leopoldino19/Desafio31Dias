b790e5f0962c3ebfa0a18a960b243f41056e8911
PedroLucas <p.leopoldino03@gmail.com>
2025-11-05 21:27:12 -0300

    teste

---
+++ estruCondicio/ex.js
@@ -0,0 +1,28 @@
+let hora = 18;
+
+if (hora < 12){
+    console.log("Bom dia!")
+}else if(hora > 18){
+    console.log("Boa tarde!")
+} else {
+    console.log("Boa noite!")
+}
+
+
+let diaSemana = 2;
+
+switch (diaSemana) {
+  case 1:
+    diaSemana <= 1
+    console.log("Segunda-feira");
+    break;
+  case 2:
+    diaSemana <=2 
+    console.log("Terça-feira");
+    break;
+  case 3:
+    console.log("Quarta-feira");
+    break;
+  default:
+    console.log("Outro dia");
+}
